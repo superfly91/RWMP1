a00768858ce726d2bd74769388fac0dc47123842
Ronan Farrell <ronan.farrell@hotmail.com>
2012-10-04 13:09:48 +0100

    Fixed player movement and locked its rotation

--- Player.js
+++ Player.js
@@ -1,30 +1,36 @@
 function Player(canvas, context, world)
 {
-	
-	this.PlayerBody = new PhysicsObject(canvas,context,world,"dynamic",0.125,0.2,5,5,0.1,0.1,0.1,"player");
+
+	this.PlayerBody = new PhysicsObject(canvas,context,world,"dynamic",0.125,0.2,5,5,0.1,1.0,0.0,"player");
+	this.speed = 4;
+	this.jumpForce = -0.05;
+	this.PlayerBody.theBody.SetFixedRotation(true);
 }
 
 Player.prototype.moveLeft = function()
 {
 	//console.log(this.PlayerBody.theBody.GetLinearVelocity().x);
-	this.PlayerBody.theBody.SetLinearVelocity(new b2Vec2(-2,this.PlayerBody.theBody.GetLinearVelocity().y));
+	this.PlayerBody.theBody.SetLinearVelocity(new b2Vec2(-this.speed,this.PlayerBody.theBody.GetLinearVelocity().y));
 };
 
 Player.prototype.moveRight = function()
 {
 	//console.log(this.PlayerBody.theBody.GetLinearVelocity().x);
-	this.PlayerBody.theBody.SetLinearVelocity(new b2Vec2(2,this.PlayerBody.theBody.GetLinearVelocity().y));
+	this.PlayerBody.theBody.SetLinearVelocity(new b2Vec2(this.speed,this.PlayerBody.theBody.GetLinearVelocity().y));
 };
 
 Player.prototype.jump = function()
 {
-	this.test_name = this.PlayerBody.theBody.GetUserData().name;
-	
-	console.log(this.test_name);
-	
 	if(this.PlayerBody.theBody.GetLinearVelocity().y < 0.001 && this.PlayerBody.theBody.GetLinearVelocity().y > -0.001)
+<<<<<<< HEAD
 		this.PlayerBody.theBody.ApplyForce(new b2Vec2(this.PlayerBody.theBody.GetLinearVelocity().x,-3));
 	
+=======
+	{
+		//this.velocity = new Vector2 theBody.GetLinearVelocity();
+		this.PlayerBody.theBody.ApplyImpulse(new b2Vec2(0,this.jumpForce), this.PlayerBody.theBody.GetWorldCenter());
+	}
+>>>>>>> Fixed player movement and locked its rotation
 };
 
 Player.prototype.duck = function()

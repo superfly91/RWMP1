33676da9f4b5ebdd94950cd4ffb0bfca5accc8a3
Ronan Farrell <ronan.farrell@hotmail.com>
2012-10-04 13:15:39 +0100

    Fixed player movement and locked rotation

--- Player.js
+++ Player.js
@@ -3,7 +3,7 @@ function Player(canvas, context, world)
 
 	this.PlayerBody = new PhysicsObject(canvas,context,world,"dynamic",0.125,0.2,5,5,0.1,1.0,0.0,"player");
 	this.speed = 4;
-	this.jumpForce = -0.05;
+	this.jumpImpulse = -0.05;
 	this.PlayerBody.theBody.SetFixedRotation(true);
 }
 
@@ -22,15 +22,10 @@ Player.prototype.moveRight = function()
 Player.prototype.jump = function()
 {
 	if(this.PlayerBody.theBody.GetLinearVelocity().y < 0.001 && this.PlayerBody.theBody.GetLinearVelocity().y > -0.001)
-<<<<<<< HEAD
-		this.PlayerBody.theBody.ApplyForce(new b2Vec2(this.PlayerBody.theBody.GetLinearVelocity().x,-3));
-	
-=======
 	{
 		//this.velocity = new Vector2 theBody.GetLinearVelocity();
-		this.PlayerBody.theBody.ApplyImpulse(new b2Vec2(0,this.jumpForce), this.PlayerBody.theBody.GetWorldCenter());
+		this.PlayerBody.theBody.ApplyImpulse(new b2Vec2(0,this.jumpImpulse), this.PlayerBody.theBody.GetWorldCenter());
 	}
->>>>>>> Fixed player movement and locked its rotation
 };
 
 Player.prototype.duck = function()
